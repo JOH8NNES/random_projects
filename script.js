95f11fc8a0a1cc38a5af24bf068e10535e7f4eaa
Johannes Lehmann <johanneslehmann19@gmail.com>
2022-12-14 20:42:20 +0100

    Div. Änderungen

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const selectElement = document.querySelector('#websites');
 
 selectElement.addEventListener('change', (event) => {
     window.open(event.target.value);
+    location.reload(true);
 })
+
 
 
 // function addwebsite() {
